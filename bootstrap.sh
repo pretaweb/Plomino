17503d5dbd1a85aad8ead123b909e13ebc23d646
Dylan Jay <software@pretaweb.com>
2025-02-25 10:18:59 +0700

    upgrade setuptools version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 [ ! -f bin/pip ] && virtualenv .
-bin/pip install --upgrade pip setuptools zc.buildout
+bin/pip install --upgrade pip setuptools zc.buildout setuptools-git setuptools-subversion 
 bin/buildout -c $1 annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
 cat annotate.txt
 cat requirements.txt
